52997fa49f6152cb390b4af13303719de9612836
Possible-99 <juanpsfi3023@gmail.com>
2022-05-02 14:34:09 -0500

    Trying a new skybox

--- Final.cpp
+++ Final.cpp
@@ -361,7 +361,7 @@ int main()
 
 		model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
-		model = glm::scale(model, glm::vec3(0.2f));
+		model = glm::scale(model, glm::vec3(3.0f));
 		staticShader.setMat4("model", model);
 		piso.Draw(staticShader);
 
